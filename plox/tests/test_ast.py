578ac45e84570799a631bca12660ad1a47ce886e
Florian-Heringa <florian_music@hotmail.com>
2025-09-04 13:53:52 +0200

    removed old, non-working code from AST generation and AST test. Became too complex to handle, especially when running into the Visitor pattern. Typing was completely out the window

--- plox/tests/test_ast.py
+++ plox/tests/test_ast.py
@@ -5,31 +5,6 @@ from plox_lib import AstPrinter
 
 from pathlib import Path
 
-# def test_ast_generation():
-
-#     here = Path(__file__).resolve().parent
-
-#     base, asts = generate_asts("Expr", [
-#         "Binary - left: Expr, operator: Token, right: Expr",
-#         "Grouping - left: Expr",
-#         "Literal - value: str | float",
-#         "Unary - operator: Token, right: Expr",
-#     ], write_stubs=True)
-
-#     assert isinstance(asts["Literal"](value=10), base)
-
-#     Binary = asts["Binary"]
-#     Literal = asts["Literal"]
-    
-#     simple_tree = Binary(
-#         left=Literal(value=10), 
-#         operator=Token(TokenType.PLUS, "+", None, 1), 
-#         right=Literal(value=5))
-    
-#     # Type checking issues here due to generated classes...
-#     assert simple_tree.left.value == 10
-#     assert simple_tree.right.value == 5
-
 def test_ast_file_generation():
 
     node = Binary(Literal(10), Token(TokenType.PLUS, "+", None, 1), Literal(5))
